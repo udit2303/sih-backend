7b22003a0c7503f5c5751b938ed063edc0c78735
udit2303 <udit2303@gmail.com>
2024-12-07 10:53:19 +0530

    add login routes

---
+++ api/routers/login.py
@@ -0,0 +1,65 @@
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel, EmailStr
+from passlib.context import CryptContext
+from core.database import db
+from models.user import User
+from bson import ObjectId
+from jose import jwt 
+import os
+from datetime import datetime, timedelta
+from core.auth import create_access_token, verify_password, hash_password
+
+# Pydantic Models
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+    refCode : int = 0
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+router = APIRouter()
+
+# Signup Route
+@router.post("/signup")
+async def signup(user: UserCreate):
+    users_collection = db.get_collection("users")
+
+    existing_user = await users_collection.find_one({"email": user.email})
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    hashed_password = hash_password(user.password)
+
+    new_user = {
+        "name": user.username,
+        "email": user.email,
+        "password": hashed_password,
+        "is_admin": True if user.refCode == 1234 else False,    
+        "created_at": datetime.now(datetime.timezone.utc)
+    }
+    result = await users_collection.insert_one(new_user)
+
+    return {"msg": "User created successfully", "user_id": str(result.inserted_id)}
+
+# Login Route
+@router.post("/login")
+async def login(user: UserLogin):
+    users_collection = db.get_collection("users")
+
+    # Find user by email
+    existing_user = await users_collection.find_one({"email": user.email})
+    if not existing_user:
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+
+    # Verify password
+    if not verify_password(user.password, existing_user["password"]):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+
+    # Create JWT token
+    access_token = create_access_token(
+        data={"sub": str(existing_user["_id"])}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
--- core/model.py
+++ core/model.py
@@ -4,35 +4,22 @@ import torch
 from transformers import AutoConfig, AutoModel
 print(torch.cuda.is_available())
 print(torch.version.cuda)
+class Model():
+    def __init__(self):
+        self.model_path = 'mPLUG/mPLUG-Owl3-2B-241014'
+        self.config = AutoConfig.from_pretrained(self.model_path, trust_remote_code=True)
+        self.model = AutoModel.from_pretrained(self.model_path, attn_implementation='sdpa', torch_dtype=torch.half, trust_remote_code=True)
+        self.model.eval().cuda()
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_path)
+        self.processor = self.model.init_processor(self.tokenizer)
 
-model_path = 'mPLUG/mPLUG-Owl3-2B-241014'
-config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
-print(config)
-# model = mPLUGOwl3Model(config).cuda().half()
-model = AutoModel.from_pretrained(model_path, attn_implementation='sdpa', torch_dtype=torch.half, trust_remote_code=True)
-model.eval().cuda()
-
-model_path = 'mPLUG/mPLUG-Owl3-2B-241014'
-tokenizer = AutoTokenizer.from_pretrained(model_path)
-processor = model.init_processor(tokenizer)
-
-image = Image.new('RGB', (500, 500), color='red')
-
-messages = [
-    {"role": "user", "content": """<|image|>
-Describe this image."""},
-    {"role": "assistant", "content": ""}
-]
-
-inputs = processor(messages, images=[image], videos=None)
-
-inputs.to('cuda')
-inputs.update({
-    'tokenizer': tokenizer,
-    'max_new_tokens':100,
-    'decode_text':True,
-})
-
-
-g = model.generate(**inputs)
-print(g)
+    def generate(self, messages, images=None, videos=None):
+        inputs = self.processor(messages,images=None, videos=None)
+        inputs.to('cuda')
+        inputs.update({
+            'tokenizer': self.tokenizer,
+            'max_new_tokens':100,
+            'decode_text':True,
+        })
+        g = self.model.generate(**inputs)
+        return g
